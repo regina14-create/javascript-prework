f3e025ee5738f6b5d7ed52dd7347896cfa82da1b
regina14-create <ivaskovaregina@gmail.com>
2025-05-31 17:56:03 +0200

    Add full computerMove logic (rock, paper, scissors)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
 }
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-var computerMove, playerMove;
-computerMove = 'kamień';
-playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove + ',to wygrywasz!');
+printMessage('Mój ruch: ' + computerMove);
